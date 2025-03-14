0012b07ba5a45e8cb5ad4f33f78c12e2d87e69a1
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-03-14 19:22:57 +0530

    new changes added

--- src/main/java/com/example/practice/Java8Programs.java
+++ src/main/java/com/example/practice/Java8Programs.java
@@ -1,9 +1,6 @@
 package com.example.practice;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -16,20 +13,57 @@ public class Java8Programs {
         /* print  characters and count of character from given string*/
         String s = "Samba shivudu ambati";
 
-        Map<String, Long> charAndCountOfChars = Arrays.stream(s.split("")).map(sChar -> sChar.toLowerCase()).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<String, Long> charAndCountOfChars = Arrays.stream(s.split(""))
+                .map(sChar -> sChar.toLowerCase())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
         System.out.println(charAndCountOfChars);
+/// //////////////////////////////////////////////////////////////////////////////////////////
+        /*print all duplicate elements from given string*/
+
+        String s2 = "samba shivudu ambati";
+
+        List<String> duplicateCharList = Arrays.stream(s2.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(map -> map.getValue() > 1)
+                .map(map -> map.getKey())
+                .collect(Collectors.toList());
+
+        System.out.println(duplicateCharList);
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*print all unique elements from given string*/
+
+        String s3 = "samba shivudu ambati";
+
+        List<String> uniqueElements = Arrays.stream(s3.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(map -> map.getValue() == 1)
+                .map(map -> map.getKey()).collect(Collectors.toList());
+
+        System.out.println(uniqueElements);
+//////////////////////////////////////////////////////////////////////////////////////////////
 
         /*print first non repeat character from given string*/
 
         String s1 = "samba shivudu ambati";
 
-        Optional<String> firstNonRepeatChar = Arrays.stream(s1.split("")).collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new, Collectors.counting())).entrySet().stream().filter(c -> c.getValue() == 1).map(m -> m.getKey()).findFirst();
+        Optional<String> firstNonRepeatChar = Arrays.stream(s1.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(c -> c.getValue() == 1)
+                .map(m -> m.getKey())
+                .findFirst();
 
         System.out.println(firstNonRepeatChar.get());
+///////////////////////////////////////////////////////////////////////////////////////////////
 
         /*slice the array with custom start and last index from
-        *given input and in the output new sliced array based on indexes
-        * */
+         *given input and in the output new sliced array based on indexes
+         * */
         int startIndex = 4;
         int lastIndex = 8;
 
@@ -38,7 +72,69 @@ public class Java8Programs {
         int[] slicedResult = Arrays.copyOfRange(originalIntArray, startIndex, lastIndex);
         /*OR*/
         int[] array = IntStream.range(startIndex, lastIndex).map(index -> originalIntArray[index]).toArray();
-        System.out.println(Arrays.toString(slicedResult));
 
+        System.out.println(Arrays.toString(slicedResult));
+/// ///////////////////////////////////////////////////////////////////////////////////////////
+
+        /*print second-highest number from given array*/
+
+        int[] numArray = {5,2,6,11,8,9};
+
+        Optional<Integer> secondHighestElement = Arrays.stream(numArray)
+                .boxed()
+                .sorted((Comparator.reverseOrder()))
+                .skip(1)
+                .findFirst();
+
+        System.out.println(secondHighestElement.get());
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*print longest string from given array*/
+
+        String[] strArray = {"samba", "shivudu", "ambati"};
+
+        String longestString = Arrays.stream(strArray)
+                .sorted((c1,c2) -> c2.length()-c1.length())
+                .findFirst().get();
+        //OR using max method
+        String longestString1 = Arrays.stream(strArray).max(Comparator.comparing(String::length)).get();
+
+        //OR using reduce method
+        String s4 = Arrays.stream(strArray).reduce((w1, w2) -> w1.length() > w2.length() ? w1 : w2).get();
+
+        System.out.println(s4);
+////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*print smallest string from given array*/
+
+        String smallestStr = Arrays.stream(strArray).min(Comparator.comparing(String::length)).get();
+        System.out.println(smallestStr);
+////////////////////////////////////////////////////////////////////////////////////////////
+
+       /*print all elements who start with 1 from given array*/
+
+       int[] intArray = {2,4,1,6,11,12,8};
+
+        List<String> numStartWith1 = Arrays.stream(intArray)
+                .boxed()
+                .map(num -> num.toString())
+                .filter(num -> num.startsWith("1"))
+                .collect(Collectors.toList());
+
+        System.out.println(numStartWith1);
+//////////////////////////////////////////////////////////////////////////////////////////
+
+        /*using String join method print all elements separated with -*/
+
+        List<String> joinStr = Arrays.asList("2", "4", "5", "6");
+        String join = String.join("-", joinStr);
+        System.out.println(join);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+        /*using limit and skip print specified values from range*/
+        List<Integer> collect = IntStream.rangeClosed(1, 6).boxed().skip(1).limit(4).collect(Collectors.toList());
+        System.out.println(collect);
+/// /////////////////////////////////////////////////////////////////////////////////////
     }
 }
